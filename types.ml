fade047f4fa9e791de62d72562d2eb740d24f3d1
wbthomason <wil.thomason@gmail.com>
2014-12-07 16:50:45 -0500

    Updated types

--- types.ml
+++ types.ml
@@ -1,16 +1,19 @@
+(* Type definitions *)
+
 (* We represent a cell by its color *)
 type cell = int;;
 
 (*
- * We represent a tile by a list of cells with coordinates.
- * The uppermost tile on the leftmost column is (0,0), and
- * all other coordinates are relative to this.
+ * We represent a tile by a grid of cells. If a cell isn't actually part of a tile,
+ * we give it color #000000.
  *)
-type tile = (cell * int * int) list;;
+type tile = cell array array;;
+
+(*We represent a board as a nxm matrix of cells *)
+type board = cell array array;;
 
 (*
- * We represent a configuration by a list of tiles with coordinates.
- * The coordinates represent the assignment of the (0,0) coordinate of each tile
- * to a real coordinate in the nxm board.
+ * We represent a configuration by a partially filled board and a list of available
+ * tiles.
  *)
-type configuration = (tile * int * int) list;;
+type configuration = (tile list) * board;;
