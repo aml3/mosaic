5dbc7b6cc9a830c607a537de0b89353adf318bee
aml3 <aml3sc@virginia.edu>
2014-12-09 22:52:09 -0500

    added todo

--- solve.ml
+++ solve.ml
@@ -38,9 +38,7 @@ let rec algorithm_x (rows : (int * (int list)) list)
     (* Find the column c with the fewest non-zero entries. *)
     let (c_j, c) = List.fold_left (fun (col_j, min_col) (j, col) ->
       if num_nonzeros col < num_nonzeros min_col 
-      then (j, col) 
-      else (col_j, min_col)
-    ) (List.hd cols) (List.tl cols) in
+      then (j, col) else (col_j, min_col)) (List.hd cols) (List.tl cols) in
     (* Randomly select a row r with a nonzero entry t from c. (I don't want to
      * deal with randomness right now, so we'll just take the first one. If it
      * works with a random selection then it should work with a deterministic
@@ -48,17 +46,15 @@ let rec algorithm_x (rows : (int * (int list)) list)
     let (r_i,r) = List.find (fun (i,row) ->
       if (List.nth row c_j) <> 0 then true else false
     ) rows in
+    (* TODO: Keep rows and cols in sync. *)
     (* Remove all rows conflicting with r. *)
     let rows = List.filter (fun (_, row) ->
       if (List.exists2 (fun row_e r_e -> row_e = r_e && row_e <> 0) row r)
-      then false
-      else true
-    ) rows in
+      then false else true) rows in
     (* Remove all columns covered by r. *)
     let cols = List.filter (fun (j,col) -> 
       let r_e = (List.nth r j) in
-      if (List.exists ((=) r_e) col) then false else true
-    ) cols in
+      if (List.exists ((=) r_e) col) then false else true) cols in
     (* Remove r. *)
     let rows = List.filter (fun (i,_) -> i <> r_i) rows in
     (* Add r to the solution and recurse. *)
