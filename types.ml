59ad8e308203fe76768c1488615016a66daf8556
aml <aml3sc@virginia.edu>
2014-12-10 13:42:18 -0500

    deleted dlx from utils

--- types.ml
+++ types.ml
@@ -20,28 +20,3 @@ type configuration = Configuration of (tile list) * board;;
 
 (* A solution is represented as an assignment of tiles to positions *)
 type solution = Solution of (tile * (int * int)) list;;
-
-(* The node type for the Dancing Links algorithm *)
-type dlx_node = {
-  mutable left : dlx_node;
-  mutable right : dlx_node;
-  mutable up :dlx_node;
-  mutable down : dlx_node;
-  mutable col_h : dlx_header;
-
-  mutable row : int;
-}
-
-and dlx_header = {
-  mutable left : dlx_header;
-  mutable right : dlx_header;
-  mutable down : dlx_header;
-  mutable count : int;
-
-}
-
-(* The sparse matrix type for the Dancing Links algorithm *)
-and dlx_matrix = {
-  head : dlx_header;
-  mutable mcount : int
-};;
